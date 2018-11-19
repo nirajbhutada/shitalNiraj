3f926ac76f3ce33c0837571d7798250473174f45
NIRAJ <nirajbhutada21@gmail.com>
2018-11-19 19:25:14 +0530

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2018, 12-1, 25, 10)}); // year, month, date, hour
+        });
+});		
+
